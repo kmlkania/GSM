5260eaccdfa8856456cfd835f70c999196c4058e
Kamil Kania <kmlkania@gmail.com>
2020-01-19 23:37:48 +0100

    test name alignment

--- test/test_gui_widgets.py
+++ test/test_gui_widgets.py
@@ -2,7 +2,6 @@ from GUIWidgets import GUIWidget
 import mock
 import pytest
 import pytestqt
-from PyQt5 import QtCore, QtWidgets
 
 
 def test_widget_can_be_started():
@@ -19,24 +18,27 @@ def test_widget_can_be_started():
 @mock.patch('PyQt5.QtWidgets.QComboBox')
 @mock.patch('PyQt5.QtWidgets.QLineEdit')
 @mock.patch('PyQt5.QtWidgets.QListWidget')
-def test_button_search_devices(q_list, q_line, q_combo, q_lbl, q_btn, q_widget):
+def test_widgets_setup(q_list, q_line, q_combo, q_lbl, q_btn, q_widget):
         with mock.patch('GUIMainWindow.GUIMainWindow') as main_window:
             with mock.patch('DeviceManager.DeviceManager'):
                 widget = GUIWidget(main_window())
                 widget.setup_head_widget()
                 widget.setup_bottom_widget()
                 widget.setup_footer_widget()
-                assert isinstance(widget, GUIWidget)
+                assert widget.head_widget_elements
+                assert widget.bottom_widget_elements
+                assert widget.footer_widget_elements
 
 
 def test_button_search_clicked(qtbot):
-        with mock.patch('DeviceManager.DeviceManager.__init__', return_value=None):
-            with mock.patch('DeviceManager.DeviceManager.set_serial_devices') as search_devices:
-                widget = GUIWidget(QtWidgets.QMainWindow())
-                widget.setup_head_widget()
-                widget.setup_bottom_widget()
-                widget.setup_footer_widget()
-                widget.setup_actions()
-                qtbot.mouseClick(widget.head_widget_elements['discover_serial_btn'], QtCore.Qt.LeftButton)
-                search_devices.assert_called_once()
+    from PyQt5 import QtCore, QtWidgets
+    with mock.patch('DeviceManager.DeviceManager.__init__', return_value=None):
+        with mock.patch('DeviceManager.DeviceManager.set_serial_devices') as search_devices:
+            widget = GUIWidget(QtWidgets.QMainWindow())
+            widget.setup_head_widget()
+            widget.setup_bottom_widget()
+            widget.setup_footer_widget()
+            widget.setup_actions()
+            qtbot.mouseClick(widget.head_widget_elements['discover_serial_btn'], QtCore.Qt.LeftButton)
+            search_devices.assert_called_once()
 
--- test/test_main_window.py
+++ test/test_main_window.py
@@ -1,10 +1,10 @@
 from GUIMainWindow import GUIMainWindow
-from PyQt5 import QtCore
 import mock
 import pytestqt
 
 
 def test_main_window_search_button_clicked(qtbot):
+    from PyQt5 import QtCore
     with mock.patch('DeviceManager.DeviceManager.__init__', return_value=None):
         with mock.patch('DeviceManager.DeviceManager.set_serial_devices') as search_devices:
             window = GUIMainWindow()
